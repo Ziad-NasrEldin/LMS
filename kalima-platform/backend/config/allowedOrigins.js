810f0d10a9f77ea8f60306bd07ab84e80faddb53
HarDizzeR <96432989+HarDizzeR@users.noreply.github.com>
2025-10-31 17:57:27 +0200

    fix cors

--- kalima-platform/backend/config/allowedOrigins.js
+++ kalima-platform/backend/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'capacitor://localhost',
     'http://localhost:5173',
     'file://',
-    'https://kalima-edu.com'
+    'https://kalima-edu.com',
+    "fekra-edu.com"
 ];
 
 module.exports = allowedOrigins;
