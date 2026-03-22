214aae72ba721d67feab1c32fae7051f2ca115b5
Ziad Ahmed <112062841+Ziad-NasrEldin@users.noreply.github.com>
2026-03-22 02:39:01 +0200

    some enhancements

--- kalima-platform/backend/middleware/loginLimiter.js
+++ kalima-platform/backend/middleware/loginLimiter.js
@@ -2,8 +2,9 @@ const rateLimit = require("express-rate-limit");
 // Prevents login attempts in a specified period of time.
 
 const loginLimiter = rateLimit({
-  windowsMs: 60 * 1000, // 1 minute should be changed for production
+  windowMs: 60 * 1000, // 1 minute should be changed for production
   max: 5, // Limit each IP to 5 login attempt per window
+  skipSuccessfulRequests: true,
   message: {
     message:
       "Too many login attempts from this IP, Please try again after a 60 second pause.",
